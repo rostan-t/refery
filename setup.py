4eeb6bd370c0f011e1b60877ed3c9530015ff354
Rostan Tabet <rostan.tabet@gmail.com>
2021-12-07 19:57:22 +0100

    refactored the setup.py script
    Signed-off-by: Rostan Tabet <rostan.tabet@gmail.com>

--- setup.py
+++ setup.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / 'README.md').read_text()
+
+NAME = 'refery'
+VERSION = '1.0.1'
+DESCRIPTION = 'Functional testing tool'
+AUTHOR = 'Rostan Tabet'
+EMAIL = 'rostan.tabet@gmail.com'
+REQUIRED = ['PyYAML', 'colorama']
 
 setup(
-    name='refery',
-    scripts=['bin/refery'],
-    version='1.0',
-    description='Generic functional testing',
-    author='Rostan Tabet',
-    author_email='rostan.tabet@gmail.com',
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
     packages=['src'],
+    install_requires=REQUIRED,
+    long_description_content_type='text/markdown',
+    long_description=README,
+    entry_points={
+        'console_scripts': [
+            'refery=src.main:main'
+        ]
+    }
 )
